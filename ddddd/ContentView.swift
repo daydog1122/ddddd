69ba15dca5379247eace8c3d558332ed0d63aba5
daydog1122 <haha4680@gmail.com>
2024-04-18 20:13:49 +0900

    Initial Commit

---
+++ ddddd/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ddddd
+//
+//  Created by 하진주 on 4/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ddddd/dddddApp.swift
@@ -0,0 +1,17 @@
+//
+//  dddddApp.swift
+//  ddddd
+//
+//  Created by 하진주 on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct dddddApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
